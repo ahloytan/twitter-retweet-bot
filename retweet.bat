449630abc7dae6dff87a8e075551fa246f8be950
Aloysius <chewinggum222@hotmail.com>
2024-03-04 19:56:56 +0800

    MSEdge doesn't register link only on first run
    SETLOCAL ENABLEDELAYEDEXPANSION allowed code to assign variable a value before evaluating

--- retweet.bat
+++ retweet.bat
@@ -6,23 +6,24 @@ set x=1
 
 :runName
 Set /P "URL=Enter tweet URL please: "
-cmd /v:on /c echo(^^!URL^^! | findstr /r "^[&^\\/?%%*:|<>\.\"]*$^" > nul
+@REM cmd /v:on /c echo(^^!URL^^! | findstr /r "^[&^\\/?%%*:|<>\.\"]*$^" > nul
 
-for /f "tokens=1,2 delims= " %%a in ("%URL%") do (
-    set Tweet=%%a
-
+@REM https://stackoverflow.com/questions/13805187/how-to-set-a-variable-inside-a-loop-for-f
+SETLOCAL ENABLEDELAYEDEXPANSION
+for /F "tokens=1,2 delims= " %%a in ("%URL%") do (
     echo."%%a" | findstr /C:"?" 1>nul
 
     if not errorlevel 1 (
-        @REM ECHO "Contains Question Mark"
-        for /f "tokens=1 delims=?" %%c in ("%Tweet%") do set "Tweet=%%c"
+        ECHO "Contains Question Mark"
+        SETLOCAL ENABLEDELAYEDEXPANSION
+        for /F "tokens=1 delims=?" %%c in ("%%a") do set "URL=%%c"
     )
 
-    robot -v URL:%Tweet% -v ADD_TEXT:%%b retweet.robot
+    robot -v URL:!URL! -v ADD_TEXT:%%b retweet.robot
 )
 
-if not defined Tweet goto :end
-if /I "%Tweet%" Equ "E" goto :end
+if not defined %URL% goto :end
+if /I "%URL%" Equ "E" goto :end
 
 
 set /a x+=1
