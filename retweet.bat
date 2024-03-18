b98d2013038ff7e4302b6c37491e1b3de51ba70e
Aloysius <chewinggum222@hotmail.com>
2024-03-18 23:49:07 +0800

    corner case for hashtagging as comment in tweet

--- retweet.bat
+++ retweet.bat
@@ -6,24 +6,23 @@ set x=1
 
 :runName
 Set /P "URL=Enter tweet URL please: "
-@REM cmd /v:on /c echo(^^!URL^^! | findstr /r "^[&^\\/?%%*:|<>\.\"]*$^" > nul
+cmd /v:on /c echo(^^!URL^^! | findstr /r "^[&^\\/?%%*:|<>\.\"]*$^" > nul
 
 @REM https://stackoverflow.com/questions/13805187/how-to-set-a-variable-inside-a-loop-for-f
-SETLOCAL ENABLEDELAYEDEXPANSION
 for /F "tokens=1,2 delims= " %%a in ("%URL%") do (
     echo."%%a" | findstr /C:"?" 1>nul
-
+    set "processedURL=%%a"
     if not errorlevel 1 (
         ECHO "Contains Question Mark"
         SETLOCAL ENABLEDELAYEDEXPANSION
-        for /F "tokens=1 delims=?" %%c in ("%%a") do set "URL=%%c"
+        for /F "tokens=1 delims=?" %%c in ("%%a") do set "processedURL=%%c"
     )
 
-    robot -v URL:!URL! -v ADD_TEXT:%%b retweet.robot
+    robot -v URL:%processedURL% -v ADD_TEXT:%%b retweet.robot
 )
 
-if not defined %URL% goto :end
-if /I "%URL%" Equ "E" goto :end
+@REM if not defined %URL% goto :end
+@REM if /I "%URL%" Equ "E" goto :end
 
 
 set /a x+=1
