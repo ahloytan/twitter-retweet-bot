11fe54fc02def7826c4c908a589f5af275f343a3
Aloysius <chewinggum222@hotmail.com>
2024-03-19 20:45:40 +0800

    corner case - url undefined on first run

--- retweet.bat
+++ retweet.bat
@@ -6,9 +6,9 @@ set x=1
 
 :runName
 Set /P "URL=Enter tweet URL please: "
-cmd /v:on /c echo(^^!URL^^! | findstr /r "^[&^\\/?%%*:|<>\.\"]*$^" > nul
 
 @REM https://stackoverflow.com/questions/13805187/how-to-set-a-variable-inside-a-loop-for-f
+SETLOCAL ENABLEDELAYEDEXPANSION
 for /F "tokens=1,2 delims= " %%a in ("%URL%") do (
     echo."%%a" | findstr /C:"?" 1>nul
     set "processedURL=%%a"
@@ -18,12 +18,8 @@ for /F "tokens=1,2 delims= " %%a in ("%URL%") do (
         for /F "tokens=1 delims=?" %%c in ("%%a") do set "processedURL=%%c"
     )
 
-    robot -v URL:%processedURL% -v ADD_TEXT:%%b retweet.robot
+    robot -v URL:!processedURL! -v ADD_TEXT:%%b retweet.robot
 )
-
-@REM if not defined %URL% goto :end
-@REM if /I "%URL%" Equ "E" goto :end
-
 
 set /a x+=1
 goto :runName
